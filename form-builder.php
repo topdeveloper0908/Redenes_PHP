1def6bd3235a3329493e4975599756f831a613c3
topdeveloper0908 <topdeveloper0908@gmail.com>
2023-04-14 19:27:01 +0200

    update

---
+++ form-builder.php
@@ -0,0 +1,155 @@
+<?php  
+session_start();
+error_reporting(0);
+
+$user = $_COOKIE['name'];
+                        
+if (strlen($user) == 0) {
+    header('location:logout');
+} else{
+    $_SESSION['user']= $_COOKIE['name'];
+    $authorization = $_COOKIE['authorization'];
+    $agency_id = $_COOKIE['agency_id'];
+    $agencies = explode("$$", $_COOKIE['agency']);
+?>
+<!-- Bootstrap core JavaScript-->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<!-- Form Builder JavaScript-->
+<script src="js/form/vkbeautify.min.js"></script>
+<script src="js/form/beauty.js"></script>
+<script src="js/form/html-beauty.js"></script>
+<script src="js/form/form-builder.min.js"></script>
+<script src="js/form/form-render.min.js"></script>
+<script src="js/form/html-form-builder.js"></script>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="icon" type="image/x-icon" href="img/icon.png">
+    <title>REDENES</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+    <div id="my-loader-element"></div>
+    <div id="my-loader-wrapper"></div>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php include ('dashboard-sidebar.php');?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <?php include ('header.php'); ?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <h1 class="h3 mb-4 text-gray-800">REDENES Form Builder</h1>
+                    <div class="well well-sm">
+                        <div id="build-wrap" style="padding: .5rem; background-color: lightgrey;">
+                        </div>
+                        <div id="build-preview" style="display: none;">
+                            <form action="#"></form>
+                        </div>
+                        <div class="text-center mb-4">
+                            <div class="btn-group" style="margin-top: 10px;" role="group">
+                                <button type="button" id="preview" class="btn btn-info">Preview</button>
+                                <button type="button" id="getHTML" class="btn btn-success">Get HTML</button>
+                                <button type="button" id="getXML" class="btn btn-success">Get XML</button>
+                                <button type="button" id="getJSON" class="btn btn-success">Get JSON</button>
+                                <button type="button" id="clear" class="btn btn-danger">Clear</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="well well-sm" id="outDiv" style="display:none;">
+                        <textarea style="height:500px;" class="form-control" id="out" name="out"></textarea>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Your Website 2023</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+<script>
+        // To show the loader
+        document.getElementById("my-loader-element").classList.add("loader");
+        
+        document.getElementById("my-loader-element").classList.remove("loader");                
+        document.getElementById("my-loader-wrapper").classList.add("d-none");
+    </script>
+</body>
+
+</html>
+<?php }  ?>
---
+++ js/form/html-form-builder.js
@@ -0,0 +1,81 @@
+$(document).ready(function (e) {
+    var formBuilderOptions = {
+        showActionButtons: false,
+        scrollToFieldOnAdd: false,
+        sortableControls: true
+    };
+
+    var formBuilder = $("#build-wrap").formBuilder(formBuilderOptions);
+
+    $("#preview").click(function (e) {
+        e.preventDefault();
+        if ($("#build-wrap").is(":visible")) {
+            $("#build-wrap").hide();
+            $("#build-preview").show();
+            $(this).text("Edit");
+
+            $("form", $("#build-preview")).formRender({
+                formData: formBuilder.formData
+            });
+        }
+        else {
+            $("#build-preview").hide();
+            $("#build-wrap").show();
+            $(this).text("Preview");
+        }
+    });
+
+    $("#getHTML").click(function (e) {
+        e.preventDefault();
+
+        $markup = $("<div/>");
+        $markup.formRender({ formData: formBuilder.actions.getData('json') });
+
+        $("#outDiv").show();
+
+        var opts = {};
+        opts.indent_size = 4;
+        opts.indent_char = " ";
+        opts.eol = "\n";
+        opts.indent_level = 0;
+        opts.indent_with_tabs = false;
+        opts.preserve_newlines = true;
+        opts.max_preserve_newlines = 5;
+        opts.jslint_happy = false;
+        opts.space_after_anon_function = false;
+        opts.brace_style = "collapse";
+        opts.keep_array_indentation = false;
+        opts.keep_function_indentation = false;
+        opts.space_before_conditional = true;
+        opts.break_chained_methods = false;
+        opts.eval_code = false;
+        opts.unescape_strings = false;
+        opts.wrap_line_length = 0;
+        opts.wrap_attributes = "auto";
+        opts.wrap_attributes_indent_size = 4;
+        opts.end_with_newline = false;
+
+        $("#out").val(html_beautify($markup.formRender("html"), opts));
+    });
+
+    $("#getXML").click(function (e) {
+        e.preventDefault();
+        $("#outDiv").show();
+        $("#out").val(vkbeautify.xml(formBuilder.actions.getData('xml'), 4));
+    });
+
+    $("#getJSON").click(function (e) {
+        e.preventDefault();
+        $("#outDiv").show();
+        $("#out").val(vkbeautify.json(formBuilder.actions.getData('json'), 4));
+    });
+
+    $("#clear").click(function (e) {
+        e.preventDefault();
+        $("#build-preview").hide();
+        $("#build-wrap").show();
+        $("#preview").text("Preview");
+        $("#out").val("");
+        formBuilder.actions.clearFields();
+    });
+});
