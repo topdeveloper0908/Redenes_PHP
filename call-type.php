e424d93e7364a25b2abc112d614dd8a5c142814d
topdeveloper0908 <topdeveloper0908@gmail.com>
2023-04-19 04:08:09 +0200

    update

--- call-type.php
+++ call-type.php
@@ -1,7 +1,6 @@
 <?php  
 session_start();
 error_reporting(0);
-
 $user = $_COOKIE['name'];
 $authorization = $_COOKIE['authorization'];
 $agency_id = $_COOKIE['agency_id'];
@@ -41,39 +40,6 @@ $agency_id = $_COOKIE['agency_id'];
 <body id="page-top">
     <div id="my-loader-element"></div>
     <div id="my-loader-wrapper"></div>
-    <script>
-        // To show the loader
-        document.getElementById("my-loader-element").classList.add("loader");
-        var mainData;
-        init_id = "<?php echo $agency_id;?>";
-        async function getData(agency_id) {
-            await $.ajax({
-                type: "GET",
-                url: "https://api.redenes.org/dev/v1/agency-call-types/",
-                data: {
-                    agency_id: agency_id,
-                    authorization: "<?php echo $authorization;?>"
-                },
-                success: function (res) {
-                    var tmp = '', tmp1 = '', tmp2 = '', tmp3 = '';
-                    var data = res.agencies_call_types;
-                    data.forEach(element => {
-                        tmp = tmp + element.id + '$$';
-                        tmp1 = tmp1 + element.call_type + '$$';
-                        tmp2 = tmp2 + element.call_abbreviation + '$$';
-                        tmp3 = tmp3 + element.active + '$$';
-                    });
-                    document.cookie = "agencies_call_types_1 = " + tmp;
-                    document.cookie = "agencies_call_types_2 = " + tmp1;
-                    document.cookie = "agencies_call_types_3 = " + tmp2;
-                    document.cookie = "agencies_call_types_4 = " + tmp3;
-                    document.getElementById("my-loader-element").classList.remove("loader");                
-                    document.getElementById("my-loader-wrapper").classList.add("d-none");
-                }
-            })
-        }
-        getData(init_id);
-    </script>
     <!-- Page Wrapper -->
     <div id="wrapper">
 
@@ -123,55 +89,6 @@ $agency_id = $_COOKIE['agency_id'];
                                         </tr>
                                     </tfoot>
                                     <tbody id="table-content">
-                                        <?php
-                                            $call_ids = explode('$$', $_COOKIE['agencies_call_types_1']);
-                                            $call_types = explode('$$', $_COOKIE['agencies_call_types_2']);
-                                            $call_abbs = explode('$$', $_COOKIE['agencies_call_types_3']);
-                                            $call_active = explode('$$', $_COOKIE['agencies_call_types_4']);
-                                            for ($i=0; $i < count($call_ids)-1; $i++) { ?>
-                                        <tr>
-                                            <td class="col-id"><?php echo $call_ids[$i];?></td>
-                                            <td>
-                                                <input type="text" class="form-control bg-light border-0 small"
-                                                placeholder="Search for..." readOnly value="<?php echo $call_types[$i]?>">
-                                            </td>
-                                            <td>
-                                                <input type="text" class="form-control bg-light border-0 small"
-                                                placeholder="Search for..." aria-label="Search"
-                                                aria-describedby="basic-addon2" value="<?php echo $call_abbs[$i]?>" readOnly>
-                                            </td>
-                                            <td>
-                                                <div class="custom-control custom-checkbox">
-                                                    <?php if($call_active[$i] == 'true'){?>
-                                                        <input type="checkbox" class="custom-control-input" id="onCallCheck<?php echo $i?>" checked disabled>
-                                                    <?php } else {?>
-                                                        <input type="checkbox" class="custom-control-input" id="onCallCheck<?php echo $i?>" disabled>
-                                                        <?php }?>
-                                                    <label class="custom-control-label" for="onCallCheck<?php echo $i?>"></label>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <button type="button" class="save-btn btn btn-success btn-icon-split my-1 mr-2 d-none">
-                                                    <span class="icon text-white-50">
-                                                        <i class="fas fa-check"></i>
-                                                    </span>
-                                                    <span class="text">Save</span>
-                                                </button>
-                                                <button type="button" class="edit-btn btn btn-success btn-icon-split my-1 mr-2">
-                                                    <span class="icon text-white-50">
-                                                        <i class="fas fa-check"></i>
-                                                    </span>
-                                                    <span class="text">Edit</span>
-                                                </button>
-                                                <button type="button" class="cancel-btn btn btn-danger btn-icon-split my-1 mr-2 d-none">
-                                                    <span class="icon text-white-50">
-                                                        <i class="fas fa-edit"></i>
-                                                    </span>
-                                                    <span class="text">Cancel</span>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                        <?php } ?>
                                     </tbody>
                                 </table>
                             </div>
@@ -213,7 +130,30 @@ $agency_id = $_COOKIE['agency_id'];
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
     <script>
         // To show the loader
-        function writeData() {
+        // To show the loader
+        document.getElementById("my-loader-element").classList.add("loader");
+        var mainData;
+        init_id = "<?php echo $agency_id;?>";
+        function getData(agency_id) {
+            $.ajax({
+                type: "GET",
+                url: "https://api.redenes.org/dev/v1/agency-call-types/",
+                data: {
+                    agency_id: agency_id,
+                    authorization: "<?php echo $authorization;?>"
+                },
+                async:false,
+                success: function (res) {
+                    var tmp = '', tmp1 = '', tmp2 = '', tmp3 = '';
+                    var data = res.agencies_call_types;
+                    writeData(data);
+                    document.getElementById("my-loader-element").classList.remove("loader");                
+                    document.getElementById("my-loader-wrapper").classList.add("d-none");
+                }
+            })
+        }
+        getData(init_id);
+        function writeData(mainData) {
             var tmp = '';
             mainData.forEach(element => {
                 tmp += "<tr><td>" + element.id + "</td><td><input type='text' class='form-control bg-light border-0 small' placeholder='Search for...' value='" + element.call_type + "' readOnly></td><td><input type='text' class='form-control bg-light border-0 small' placeholder='Search for...' value='" + element.call_abbreviation + "' readOnly></td><td><div class='custom-control custom-checkbox'><input type='checkbox' class='custom-control-input' id='check'" + (element.active == "false" ? "" : "checked") + " disabled><label class='custom-control-label' for='check'></label></div></td><td><button type='button' class='btn btn-success btn-icon-split my-1 mr-2 btn-save'><span class='icon text-white-50'><i class='fas fa-check'></i></span><span class='text'>Save</span></button><button type='button' class='btn btn-danger btn-icon-split my-1 mr-2 d-none btn-cancel'><span class='icon text-white-50'><i class='fas fa-edit'></i></span><span class='text'>Cancel</span></button></td></tr>";
--- users.php
+++ users.php
@@ -43,47 +43,6 @@ $agency_id = $_COOKIE['agency_id'];
     <div id="my-loader-wrapper"></div>
     <!-- Page Wrapper -->
     <div id="wrapper">
-    <script>
-        // To show the loader
-        document.getElementById("my-loader-element").classList.add("loader");
-        var mainData;
-        init_id = "<?php echo $agency_id;?>";
-        async function getData(agency_id) {
-            await $.ajax({
-                type: "GET",
-                url: "https://api.redenes.org/dev/v1/agency-users/",
-                data: {
-                    agency_id: agency_id,
-                    authorization: "<?php echo $authorization;?>"
-                },
-                success: function (res) {
-                    var tmp = '', tmp1 = '', tmp2 = '', tmp3 = '', tmp4 = '', tmp5 = '', tmp6 = '', tmp7 = '';
-                    var data = res.agencies_users;
-                    data.forEach(element => {
-                        tmp = tmp + element.id + '$$';
-                        tmp1 = tmp1 + element.name + '$$';
-                        tmp2 = tmp2 + element.status + '$$';
-                        tmp3 = tmp3 + element.type + '$$';
-                        tmp4 = tmp4 + element.medical + '$$';
-                        tmp5 = tmp5 + element.join_date + '$$';
-                        tmp6 = tmp6 + element.last_login + '$$';
-                        tmp7 = tmp7 + element.admin + '$$';
-                    });
-                    document.cookie = "agencies_users_1 = " + tmp;
-                    document.cookie = "agencies_users_2 = " + tmp1;
-                    document.cookie = "agencies_users_3 = " + tmp2;
-                    document.cookie = "agencies_users_4 = " + tmp3;
-                    document.cookie = "agencies_users_5 = " + tmp4;
-                    document.cookie = "agencies_users_6 = " + tmp5;
-                    document.cookie = "agencies_users_7 = " + tmp6;
-                    document.cookie = "agencies_users_8 = " + tmp7;
-                    document.getElementById("my-loader-element").classList.remove("loader");                
-                    document.getElementById("my-loader-wrapper").classList.add("d-none");
-                }
-            })
-        }
-        getData(init_id);
-    </script>
 
         <!-- Sidebar -->
         <?php include ('sidebar.php');?>
@@ -138,78 +97,8 @@ $agency_id = $_COOKIE['agency_id'];
                                             <th style="width: 14rem;">Edit</th>
                                         </tr>
                                     </tfoot>
-                                    <tbody>
-                                        <?php
-                                            $users_ids = explode('$$', $_COOKIE['agencies_users_1']);
-                                            $users_names = explode('$$', $_COOKIE['agencies_users_2']);
-                                            $users_status = explode('$$', $_COOKIE['agencies_users_3']);
-                                            $users_type = explode('$$', $_COOKIE['agencies_users_4']);
-                                            $users_medical = explode('$$', $_COOKIE['agencies_users_5']);
-                                            $users_join = explode('$$', $_COOKIE['agencies_users_6']);
-                                            $users_last_login = explode('$$', $_COOKIE['agencies_users_7']);
-                                            $users_admin = explode('$$', $_COOKIE['agencies_users_8']);
-                                            for ($i=0; $i < count($users_ids)-1; $i++) { ?>
-                                        <tr>
-                                            <td class="col-id"><?php echo $users_ids[$i];?></td>
-                                            <td>
-                                                <input type="text" class="form-control bg-light border-0 small"
-                                                placeholder="Search for..." aria-label="Search"
-                                                aria-describedby="basic-addon2" readOnly value="<?php echo $users_names[$i]?>">
-                                            </td>
-                                            <td>
-                                                <select name="dataTable_length" aria-controls="dataTable" class="custom-select form-control form-control-sm" disabled>
-                                                    <option value="Available" <?php if($users_status[$i] == 'Available') {echo 'selected';}?>>Available</option>
-                                                    <option value="On Call" <?php if($users_status[$i] == 'On Call') {echo 'selected';}?>>On Call</option>
-                                                    <option value="On Duty" <?php if($users_status[$i] == 'On Duty') {echo 'selected';}?>>On Duty</option>
-                                                    <option value="Off Duty" <?php if($users_status[$i] == 'Off Duty') {echo 'selected';}?>>Off Duty</option>
-                                                </select>
-                                            </td>
-                                            <td>
-                                                <select name="dataTable_length" aria-controls="dataTable" class="custom-select form-control form-control-sm" disabled>
-                                                    <option value="Chief" <?php if($users_type[$i] == 'Chief') {echo 'selected';}?>>Chief</option>
-                                                    <option value="Operations Leader" <?php if($users_type[$i] == 'Operations Leader') {echo 'selected';}?>>Operations Leader</option>
-                                                    <option value="Member" <?php if($users_type[$i] == 'Member') {echo 'selected';}?>>Member</option>
-                                                    <option value="Support Member" <?php if($users_type[$i] == 'Support Member') {echo 'selected';}?>>Support Member</option>
-                                                </select>
-                                            </td>
-                                            <td>
-                                            <select name="dataTable_length" aria-controls="dataTable" class="custom-select form-control form-control-sm" disabled>
-                                                <option value="EMT" <?php if($users_meidcal[$i] == 'EMT') {echo 'selected';}?>>EMT</option>
-                                                <option value="Paramedic" <?php if($users_meidcal[$i] == 'Paramedic') {echo 'selected';}?>>Paramedic</option>
-                                                <option value="Nurse" <?php if($users_meidcal[$i] == 'Nurse') {echo 'selected';}?>>Nurse</option>
-                                                <option value="Doctor" <?php if($users_meidcal[$i] == 'Doctor') {echo 'selected';}?>>Doctor</option>
-                                            </select>
-                                            </td>
-                                            <td><?php echo $users_join[$i];?></td>
-                                            <td><?php echo $users_last_login[$i];?></td>
-                                            <td>
-                                                <div class="custom-control custom-checkbox">
-                                                    <input type="checkbox" class="custom-control-input" id="onCallCheck<?php echo $i;?>" disabled <?php if($users_admin[$i] == 'true'){echo 'checked';}?>>
-                                                    <label class="custom-control-label" for="onCallCheck<?php echo $i;?>"></label>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <button type="button" class="save-btn btn btn-success btn-icon-split my-1 mr-2 d-none">
-                                                    <span class="icon text-white-50">
-                                                        <i class="fas fa-check"></i>
-                                                    </span>
-                                                    <span class="text">Save</span>
-                                                </button>
-                                                <button type="button" class="edit-btn btn btn-success btn-icon-split my-1 mr-2">
-                                                    <span class="icon text-white-50">
-                                                        <i class="fas fa-check"></i>
-                                                    </span>
-                                                    <span class="text">Edit</span>
-                                                </button>
-                                                <button type="button" class="cancel-btn btn btn-danger btn-icon-split my-1 mr-2 d-none">
-                                                    <span class="icon text-white-50">
-                                                        <i class="fas fa-edit"></i>
-                                                    </span>
-                                                    <span class="text">Cancel</span>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                        <?php }?>
+                                    <tbody id="table-content">
+                                        
                                     </tbody>
                                 </table>
                             </div>
@@ -268,6 +157,78 @@ $agency_id = $_COOKIE['agency_id'];
     <script src="js/main.js"></script>
 
     <script>        
+
+        document.getElementById("my-loader-element").classList.add("loader");
+        init_id = "<?php echo $agency_id;?>";
+        function getData(agency_id) {
+            $.ajax({
+                type: "GET",
+                url: "https://api.redenes.org/dev/v1/agency-users/",
+                data: {
+                    agency_id: agency_id,
+                    authorization: "<?php echo $authorization;?>"
+                },
+                async:false,
+                success: function (res) {
+                    var data = res.agencies_users;
+                    writeData(data);
+                    document.getElementById("my-loader-element").classList.remove("loader");                
+                    document.getElementById("my-loader-wrapper").classList.add("d-none");
+                }
+            })
+        }
+        getData(init_id);
+        function writeData(mainData) {
+            console.log(mainData);
+            var tmp = '';
+            mainData.forEach(element => {
+                tmp += "<tr><td class='col-id'>"+element.id+"</td><td><input type='text' class='form-control bg-light border-0 small' placeholder='Search for...' aria-label='Search' aria-describedby='basic-addon2' readOnly value='"+element.name+"'></td><td><select name='dataTable_length' aria-controls='dataTable' class='custom-select form-control form-control-sm' disabled><option value='Available" +element.status=='Available'?'selected':''+ ">Available</option><option value='On Call" +element.status=='On Call'?'selected':''+ ">On Call</option><option value='On Duty" +element.status=='On Duty'?'selected':''+ ">On Duty</option><option value='Off Duty" +element.status=='Off Duty'?'selected':''+ ">Off Duty</option></select></td><td><select name='dataTable_length' aria-controls='dataTable' class='custom-select form-control form-control-sm' disabled><option value='Chief'"+element.status=='Chief'?'selected':''+">Chief</option>
+                            <option value="Operations Leader" <?php if($users_type[$i] == 'Operations Leader') {echo 'selected';}?>>Operations Leader</option>
+                            <option value="Member" <?php if($users_type[$i] == 'Member') {echo 'selected';}?>>Member</option>
+                            <option value="Support Member" <?php if($users_type[$i] == 'Support Member') {echo 'selected';}?>>Support Member</option>
+                        </select>
+                    </td>
+                    <td>
+                    <select name="dataTable_length" aria-controls="dataTable" class="custom-select form-control form-control-sm" disabled>
+                        <option value="EMT" <?php if($users_meidcal[$i] == 'EMT') {echo 'selected';}?>>EMT</option>
+                        <option value="Paramedic" <?php if($users_meidcal[$i] == 'Paramedic') {echo 'selected';}?>>Paramedic</option>
+                        <option value="Nurse" <?php if($users_meidcal[$i] == 'Nurse') {echo 'selected';}?>>Nurse</option>
+                        <option value="Doctor" <?php if($users_meidcal[$i] == 'Doctor') {echo 'selected';}?>>Doctor</option>
+                    </select>
+                    </td>
+                    <td><?php echo $users_join[$i];?></td>
+                    <td><?php echo $users_last_login[$i];?></td>
+                    <td>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="onCallCheck<?php echo $i;?>" disabled <?php if($users_admin[$i] == 'true'){echo 'checked';}?>>
+                            <label class="custom-control-label" for="onCallCheck<?php echo $i;?>"></label>
+                        </div>
+                    </td>
+                    <td>
+                        <button type="button" class="save-btn btn btn-success btn-icon-split my-1 mr-2 d-none">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <span class="text">Save</span>
+                        </button>
+                        <button type="button" class="edit-btn btn btn-success btn-icon-split my-1 mr-2">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-check"></i>
+                            </span>
+                            <span class="text">Edit</span>
+                        </button>
+                        <button type="button" class="cancel-btn btn btn-danger btn-icon-split my-1 mr-2 d-none">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-edit"></i>
+                            </span>
+                            <span class="text">Cancel</span>
+                        </button>
+                    </td>
+                </tr>
+                // tmp += "<tr><td>" + element.id + "</td><td><input type='text' class='form-control bg-light border-0 small' placeholder='Search for...' value='" + element.call_type + "' readOnly></td><td><input type='text' class='form-control bg-light border-0 small' placeholder='Search for...' value='" + element.call_abbreviation + "' readOnly></td><td><div class='custom-control custom-checkbox'><input type='checkbox' class='custom-control-input' id='check'" + (element.active == "false" ? "" : "checked") + " disabled><label class='custom-control-label' for='check'></label></div></td><td><button type='button' class='btn btn-success btn-icon-split my-1 mr-2 btn-save'><span class='icon text-white-50'><i class='fas fa-check'></i></span><span class='text'>Save</span></button><button type='button' class='btn btn-danger btn-icon-split my-1 mr-2 d-none btn-cancel'><span class='icon text-white-50'><i class='fas fa-edit'></i></span><span class='text'>Cancel</span></button></td></tr>";
+            });
+            document.getElementById('table-content').innerHTML = tmp;
+        }
 
         const editButtons = document.querySelectorAll('.edit-btn');
         const saveButtons = document.querySelectorAll('.save-btn');
