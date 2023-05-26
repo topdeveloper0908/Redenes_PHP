9282db277ef8dd2bf0cc37b5f6a4657dcae7865d
topdeveloper <topdeveloper0908@gmail.com>
2023-05-26 22:55:51 +0200

    update

--- module-setting.php
+++ module-setting.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 session_start();
 error_reporting(0);
 
@@ -23,9 +23,7 @@ $agency_id = $_COOKIE['agency_id'];
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
@@ -39,7 +37,7 @@ $agency_id = $_COOKIE['agency_id'];
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <?php include ('sidebar.php');?>
+        <?php include('sidebar.php'); ?>
         <!-- End of Sidebar -->
 
         <!-- Content Wrapper -->
@@ -49,7 +47,7 @@ $agency_id = $_COOKIE['agency_id'];
             <div id="content">
 
                 <!-- Topbar -->
-                <?php include ('header.php'); ?>
+                <?php include('header.php'); ?>
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
@@ -62,7 +60,7 @@ $agency_id = $_COOKIE['agency_id'];
                         <div class="card shadow mb-4" style="max-width: 1400px">
                             <div class="card-header py-3 d-flex align-items-center justify-content-between">
                                 <div class="d-flex align-items-center">
-                                    <h6 class="mr-3 mb-0" style="white-space:nowrap">User Group</h6>
+                                    <h6 class="mr-3 mb-0" style="white-space:nowrap">Agency Type</h6>
                                     <select onchange="changeUserGroup(event)" id="userGroupDropdown" name='dataTable_length' aria-controls='dataTable' class='custom-select form-control form-control-sm' style="width: 13rem">
                                     </select>
                                 </div>
@@ -97,6 +95,7 @@ $agency_id = $_COOKIE['agency_id'];
                                                 <th style="width: 8rem;">View</th>
                                                 <th style="width: 8rem;">Edit</th>
                                                 <th style="width: 8rem;">Add</th>
+                                                <th>Default Format</th>
                                             </tr>
                                         </thead>
                                         <tfoot>
@@ -106,6 +105,7 @@ $agency_id = $_COOKIE['agency_id'];
                                                 <th style="width: 8rem;">View</th>
                                                 <th style="width: 8rem;">Edit</th>
                                                 <th style="width: 8rem;">Add</th>
+                                                <th>Default Format</th>
                                             </tr>
                                         </tfoot>
                                         <tbody id="table-content">
@@ -113,22 +113,10 @@ $agency_id = $_COOKIE['agency_id'];
                                                 <td>Home</td>
                                                 <td>Directory</td>
                                                 <td>
-                                                    <div class="custom-control custom-checkbox small">
-                                                        <input type="checkbox" class="custom-control-input" id="DirectoryCheck1" disabled>
-                                                        <label class="custom-control-label" for="DirectoryCheck1"></label>
-                                                    </div>
                                                 </td>
                                                 <td>
-                                                    <div class="custom-control custom-checkbox small">
-                                                        <input type="checkbox" class="custom-control-input" id="DirectoryCheck2" disabled>
-                                                        <label class="custom-control-label" for="DirectoryCheck2"></label>
-                                                    </div>
                                                 </td>
                                                 <td>
-                                                    <div class="custom-control custom-checkbox small">
-                                                        <input type="checkbox" class="custom-control-input" id="DirectoryCheck3" disabled>
-                                                        <label class="custom-control-label" for="DirectoryCheck3"></label>
-                                                    </div>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -177,44 +165,31 @@ $agency_id = $_COOKIE['agency_id'];
     <script>
         // To show the loader
         document.getElementById("my-loader-element").classList.add("loader");
-        var module_setting;
-        init_id = "<?php echo $agency_id;?>";
+        var module_setting, module_setting_2;
+        init_id = "<?php echo $agency_id; ?>";
+
         function getData(agency_id) {
             $.ajax({
                 type: "GET",
                 url: "https://api.redenes.org/dev/v1/agency-module-settings/",
                 data: {
                     agency_id: agency_id,
-                    authorization: "<?php echo $authorization;?>"
+                    authorization: "<?php echo $authorization; ?>"
                 },
-                success: function (res) {
+                success: function(res) {
                     module_setting = res.agencies_module_settings[0];
-                    writeDropdown(res.user_group_selected, res.user_groups);
-                    writeTable(res.agencies_module_settings[0]);
+                    writeDropdown(res.agency_type_selected, res.agency_types);
+                    writeTable();
                     // To hide the loader
-                    document.getElementById("my-loader-element").classList.remove("loader");                
+                    document.getElementById("my-loader-element").classList.remove("loader");
                     document.getElementById("my-loader-wrapper").classList.add("d-none");
                 }
             })
         }
         getData(init_id);
+
         function saveData() {
-            var authorization = "<?php echo $authorization;?>";
-            for(let key in module_setting) {
-                for(let subkey in module_setting[key][0]) {
-                    for (let i = 0; i < 3; i++) {
-                        if(i == 0) {
-                            module_setting[key][0][subkey][0].view = document.getElementById(subkey+'Check'+i).checked?'true':'false';
-                        }
-                        else if(i == 1) {
-                            module_setting[key][0][subkey][0].view = document.getElementById(subkey+'Check'+i).checked?'true':'false';
-                        }
-                        else {
-                            module_setting[key][0][subkey][0].view = document.getElementById(subkey+'Check'+i).checked?'true':'false';
-                        }
-                    }
-                }
-            }
+            var authorization = "<?php echo $authorization; ?>";
             var formData = {
                 authorization: authorization.toString(),
                 agency_id: init_id,
@@ -225,100 +200,136 @@ $agency_id = $_COOKIE['agency_id'];
                 url: "https://api.redenes.org/dev/v1/agency-module-settings/",
                 data: JSON.stringify(formData),
                 dataType: "json",
-                contentType:'application/json',
-                success: function (res) {
+                contentType: 'application/json',
+                success: function(res) {
+                    console.log(res);
                     document.getElementById("edit-btn").classList.remove("d-none");
                     document.getElementById("save-btn").classList.add("d-none");
                     document.getElementById("cancel-btn").classList.add("d-none");
-                    document.getElementById('userGroupDropdown').removeAttribute("disabled");
-                    var inputs = document.querySelectorAll('.custom-control-input');
-                    inputs.forEach(element => {
+                    var selects = document.querySelectorAll('.custom-select');
+                    selects.forEach(element => {
                         element.setAttribute("disabled", true);
                     });
+                    document.getElementById('userGroupDropdown').removeAttribute("disabled");
                 }
             })
         }
+
         function saveEnable() {
-            var inputs = document.querySelectorAll('.custom-control-input');
-            inputs.forEach(element => {
-                if(element.classList.contains('always-check') == false) {
-                    element.removeAttribute("disabled");
-                }
+            var selects = document.querySelectorAll('.custom-select');
+            selects.forEach(element => {
+                element.removeAttribute("disabled");
             });
             document.getElementById('userGroupDropdown').setAttribute("disabled", true);
             document.getElementById("edit-btn").classList.add("d-none");
             document.getElementById("save-btn").classList.remove("d-none");
             document.getElementById("cancel-btn").classList.remove("d-none");
+            module_setting_2 = structuredClone(module_setting);
         }
+
         function cancelSave() {
-            writeTable(module_setting);
+            module_setting = module_setting_2;
+            writeTable();
             document.getElementById("edit-btn").classList.remove("d-none");
             document.getElementById("save-btn").classList.add("d-none");
             document.getElementById("cancel-btn").classList.add("d-none");
-            document.getElementById('userGroupDropdown').removeAttribute("disabled");
-            var inputs = document.querySelectorAll('.custom-control-input');
-            inputs.forEach(element => {
+            var selects = document.querySelectorAll('.custom-select');
+            selects.forEach(element => {
                 element.setAttribute("disabled", true);
             });
+            document.getElementById('userGroupDropdown').removeAttribute("disabled");
         }
-        function writeTable(data){
+
+        function writeTable() {
+            data = module_setting;
             tmp = '';
+            var status = ['enabled', 'disabled', 'true', 'false'];
             for (let key in data) {
                 new_item = 0;
-                for(let subkey in data[key][0]) {
-                    if(new_item == 0 && key !='home') {
-                        tmp += "<tr><td></td><td></td><td></td><td></td><td></td></tr>";    
+                for (let subkey in data[key][0]) {
+                    element = data[key][0][subkey][0];
+                    if (new_item == 0 && key != 'home') {
+                        tmp += "<tr><td></td><td></td><td></td><td></td><td></td></tr>";
                         new_item++;
                     }
                     tmp += "<tr>";
-                    tmp += "<td style='text-transform:capitalize;'>"+key+"</td>";
-                    tmp += "<td style='text-transform:capitalize;'>"+subkey.replace('_', ' ')+"</td>";
-                    for (let i = 0; i < 3; i++) {
-                        tmp += "<td><div class='custom-control custom-checkbox'><input type='checkbox' class='custom-control-input";
-                        if(data[key][0][subkey][0].add == 'always') {
-                            tmp += " always-check";    
+                    tmp += "<td style='text-transform:capitalize;'>" + key + "</td>";
+                    tmp += "<td style='text-transform:capitalize;'>" + subkey.replace('_', ' ') + "</td>";
+                    tmp += "<td><select onchange=valueChange(event) data-action='view' data-key=" + key + " data-subKey=" + subkey + " name='dataTable_length' aria-controls='dataTable' class='custom-select form-control form-control-sm' disabled>"
+                    for (let index = 0; index < 4; index++) {
+                        tmp += "<option value='" + status[index] + "'";
+                        if (element.view == status[index]) {
+                            tmp += " selected";
                         }
-                        tmp += "' id='"+subkey+"Check"+i+"' disabled ";
-                        if(data[key][0][subkey][0].add != 'false' && i == 0) {
-                            tmp +=" checked";    
-                        }
-                        if(data[key][0][subkey][0].edit != 'false' && i == 1) {
-                            tmp +=" checked";    
-                        }
-                        if(data[key][0][subkey][0].view != 'false' && i == 2) {
-                            tmp +=" checked";    
-                        }
-                        tmp +="><label class='custom-control-label' for='"+subkey+"Check"+i+"'></label></div></td>";
+                        tmp += ">" + status[index] + "</option>"
                     }
+                    tmp += "</td>";
+                    tmp += "<td><select onchange=valueChange(event) data-action='edit' data-key=" + key + " data-subKey=" + subkey + " name='dataTable_length' aria-controls='dataTable' class='custom-select form-control form-control-sm' disabled>"
+                    for (let index = 0; index < 4; index++) {
+                        tmp += "<option value='" + status[index] + "'";
+                        if (element.edit == status[index]) {
+                            tmp += " selected";
+                        }
+                        tmp += ">" + status[index] + "</option>"
+                    }
+                    tmp += "</td>";
+                    tmp += "<td><select onchange=valueChange(event) data-action='add' data-key=" + key + " data-subKey=" + subkey + " name='dataTable_length' aria-controls='dataTable' class='custom-select form-control form-control-sm' disabled>"
+                    for (let index = 0; index < 4; index++) {
+                        tmp += "<option value='" + status[index] + "'";
+                        if (element.add == status[index]) {
+                            tmp += " selected";
+                        }
+                        tmp += ">" + status[index] + "</option>"
+                    }
+                    tmp += "</td>";
+                    tmp += "<td><select onchange=valueChange(event) data-action='default_form_selected' data-key=" + key + " data-subKey=" + subkey + " name='dataTable_length' aria-controls='dataTable' class='custom-select form-control form-control-sm' disabled>"
+                    for (let index = 0; index < element.default_forms.length; index++) {
+                        tmp += "<option value='" + element.default_forms[index] + "'";
+                        if (element.default_form_selected == element.default_forms[index]) {
+                            tmp += " selected";
+                        }
+                        tmp += ">" + element.default_forms[index] + "</option>"
+                    }
+                    tmp += "</td>";
                     tmp += "</tr>";
                 }
             }
             document.getElementById('table-content').innerHTML = tmp;
         }
-        function writeDropdown(selected, options){
+
+        function valueChange(e) {
+            value = e.currentTarget.value;
+            action = e.currentTarget.getAttribute('data-action');
+            key = e.currentTarget.getAttribute('data-key');
+            subkey = e.currentTarget.getAttribute('data-subKey');
+            module_setting[key][0][subkey][0][action] = value;
+        }
+
+        function writeDropdown(selected, options) {
             tmp = '';
             for (let index = 0; index < options.length; index++) {
                 const element = options[index];
-                tmp +="<option value='"+options[index]+"'";
-                if(options[index] == selected) 
-                    tmp +=" selected";
-                tmp += ">"+options[index]+"</option>";
-            }       
+                tmp += "<option value='" + options[index] + "'";
+                if (options[index] == selected)
+                    tmp += " selected";
+                tmp += ">" + options[index] + "</option>";
+            }
             document.getElementById('userGroupDropdown').innerHTML = tmp;
         }
+
         function changeUserGroup(e) {
             $.ajax({
                 type: "GET",
                 url: "https://api.redenes.org/dev/v1/agency-module-settings/",
                 data: {
                     agency_id: init_id,
-                    authorization: "<?php echo $authorization;?>",
+                    authorization: "<?php echo $authorization; ?>",
                     user_groups: e.target.value
                 },
-                success: function (res) {
+                success: function(res) {
                     module_setting = res.agencies_module_settings[0];
                     writeDropdown(res.user_group_selected, res.user_groups);
-                    writeTable(res.agencies_module_settings[0]);
+                    writeTable();
                 }
             })
         }
