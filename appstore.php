ac979e73c396f9eefab79d63ea580d3ca9b6b8e7
topdeveloper0908 <topdeveloper0908@gmail.com>
2023-06-21 22:06:09 +0200

    update

--- appstore.php
+++ appstore.php
@@ -8,19 +8,18 @@ session_start();
             function getDevice() {
                 var phoneModel;
                 var phoneVersion;    
+                console.log()
                 if(navigator.userAgent.toLowerCase().match(/(iphone|ipad|ipod|android)/) == null) {
                     phoneModel = 'Null';
                     phoneVersion = 'Null';    
                     window.location = "https://www.redenes.org";
                 }
                 else {
-                    phoneModel = navigator.userAgent.toLowerCase().match(/(iphone|ipad|ipod|android)/)[1];
-                    phoneVersion = navigator.userAgent.toLowerCase().match(/(iphone os|cpu os|android) ([\d_]+)/)[2];
-                    if(phoneModel == 'android') {
-                        window.location = "https://www.google.com";
+                    if(navigator.userAgent.toLowerCase().match(/(android)/) == null) {
+                        window.location = "https://www.apple.com";
                     }
                     else {
-                        window.location = "https://www.apple.com";
+                        window.location = "https://www.google.com";
                     }
                 }
             }
